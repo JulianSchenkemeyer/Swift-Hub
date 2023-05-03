14cbd89e52e6a380551855bf41d32add884e9be3
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2023-05-03 08:01:15 +0200

    Setup a new project for the SwiftUI variant of SwiftHub

---
+++ Swift-Hub-SwiftUI/Swift-Hub-SwiftUI/Swift_Hub_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Hub_SwiftUIApp.swift
+//  Swift-Hub-SwiftUI
+//
+//  Created by Julian Schenkemeyer on 03.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Hub_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
